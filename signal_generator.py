11358f0c5031bc08b1a9d2a2ac24ac20f869f449
Kuba <janochajakub02@gmail.com>
2025-03-24 20:45:58 +0100

    modified sinus functions

--- signal_generator.py
+++ signal_generator.py
@@ -15,5 +15,25 @@ def sinus(amplitude, period, start, duration, sample_rate=1000):
     time = np.linspace(start, duration, num=sample_rate)
     sinus = []
     for i in range(len(time)):
-        sinus.append(amplitude * np.sin(((2 * np.pi)/period)*(i-start)))
+        sinus.append(amplitude * np.sin(((2 * np.pi) / period) * (i - start)))
+    return sinus, time
+
+
+def sinus_abs(amplitude, period, start, duration, sample_rate=1000):
+    time = np.linspace(start, duration, num=sample_rate)
+    sinus = []
+    for i in range(len(time)):
+        sinus.append(
+            amplitude * abs(np.sin(((2 * np.pi) / period) * (i - start))))
+    return sinus, time
+
+
+def sinus_one_half(amplitude, period, start, duration, sample_rate=1000):
+    time = np.linspace(start, duration, num=sample_rate)
+    sinus = []
+    for i in range(len(time)):
+        sinus.append(
+            (amplitude * ((np.sin(((2 * np.sin) / period) * (i - start))) +
+                          abs((np.sin(
+                              ((2 * np.sin) / period) * (i - start)))))) / 2)
     return sinus, time
