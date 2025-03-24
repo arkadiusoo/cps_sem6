18cd0b4c44d9c10ddbd58420d9f2220d0e2d0b1f
Kuba <janochajakub02@gmail.com>
2025-03-24 23:10:25 +0100

    all unitl jump

--- signal_generator.py
+++ signal_generator.py
@@ -8,7 +8,7 @@ def uniform_dist_noise(amplitude, start, duration, sample_rate=None):
     j = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             value = np.random.uniform(-amplitude, amplitude)
         noise.append([value, i])
         if sample_rate is not None:
@@ -29,7 +29,7 @@ def gauss_noise(amplitude, start, duration, sample_rate=None):
     j = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             value = np.random.normal(0, amplitude)
         if sample_rate is not None:
             i += 1 / (3 * sample_rate)
@@ -49,7 +49,7 @@ def sinus(amplitude, period, start, duration, sample_rate=None):
     j = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             value = sinus.append(
                 amplitude * np.sin(((2 * np.pi) / period) * (i - start)))
         if sample_rate is not None:
@@ -70,7 +70,7 @@ def sinus_abs(amplitude, period, start, duration, sample_rate=None):
     j = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             sinus.append(
                 amplitude * abs(np.sin(((2 * np.pi) / period) * (i - start))))
         if sample_rate is not None:
@@ -91,7 +91,7 @@ def sinus_one_half(amplitude, period, start, duration, sample_rate=None):
     j = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             sinus.append(
                 (amplitude * (
                         (np.sin(((2 * np.sin) / period) * (i - start))) +
@@ -117,13 +117,13 @@ def square_classic(amplitude, period, start, duration, kw, sample_rate=None):
     k = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             if (k * period + start <= i
                     < kw * period + k * period + start):
                 value = amplitude
             if i % (2 * kw) == 0:
                 k += 1
-            square.append(value)
+            square.append([value, i])
         if sample_rate is not None:
             i += 1 / (3 * sample_rate)
             j += 1
@@ -143,14 +143,14 @@ def square_simetric(amplitude, period, start, duration, kw, sample_rate=None):
     k = 0
     while i < duration:
         value = 0
-        if i < start:
+        if i > start:
             value = -amplitude
             if (k * period + start <= i
                     < kw * period + k * period + start):
                 value = amplitude
             if i % (2 * kw) == 0:
                 k += 1
-            square.append(value)
+            square.append([value, i])
         if sample_rate is not None:
             i += 1 / (3 * sample_rate)
             j += 1
@@ -163,27 +163,54 @@ def square_simetric(amplitude, period, start, duration, kw, sample_rate=None):
 
 
 def triangular(amplitude, period, start, duration, kw, sample_rate=None):
-    square = []
-    user_square = []
+    triangle = []
+    user_triangle = []
     i = 0
     j = 0
     k = 0
     while i < duration:
         value = 0
-        if i < start:
-            value = -amplitude
+        if i > start:
+            value = (-amplitude / (period * (1 - kw))) * (
+                        i - k * period - start) + (amplitude / (1 - kw))
             if (k * period + start <= i
                     < kw * period + k * period + start):
-                value = amplitude
+                value = (amplitude / (kw * period)) * (
+                            i - (k * period) - start)
             if i % (2 * kw) == 0:
                 k += 1
-            square.append(value)
+            triangle.append([value, i])
         if sample_rate is not None:
             i += 1 / (3 * sample_rate)
             j += 1
             if j % 3 == 0:
-                user_square.append([value, i])
+                user_triangle.append([value, i])
                 j = 0
         else:
             sample_rate += 1 / 1000
-    return square, user_square
+    return triangle, user_triangle
+
+
+def jump_signal(amplitude, start, duration, jump_time, sample_rate=None):
+    jump = []
+    user_jump = []
+    i = 0
+    j = 0
+    while i < duration:
+        value = 0
+        if i > start:
+            if i == jump_time:
+                value = amplitude/2
+            else:
+                value = amplitude
+
+            jump.append([value, i])
+        if sample_rate is not None:
+            i += 1 / (3 * sample_rate)
+            j += 1
+            if j % 3 == 0:
+                user_jump.append([value, i])
+                j = 0
+        else:
+            sample_rate += 1 / 1000
+    return jump, user_jump
